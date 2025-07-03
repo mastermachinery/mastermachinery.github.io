7b708b3f3edb9276c9c7be026b1cb6c3c91f4e98
Barryetter <87667493+Barryetter@users.noreply.github.com>
2025-07-03 16:29:28 -0400

    Update ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -4,5 +4,8 @@
     <p>Email: info@mastermachinerywv.com</p>
     <p>Phone: (123) 456-7890</p>
   </div>
+  <div class="container mt-5">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d71803.07707822407!2d-81.1866939883687!3d37.31224991075589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x884e6b25ac6316f9%3A0xf1d879652ce15b!2s260%20Autumn%20Ln%2C%20Princeton%2C%20WV%2024739!5e0!3m2!1sen!2sus!4v1751574379135!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
 </template>
 <script setup></script>
