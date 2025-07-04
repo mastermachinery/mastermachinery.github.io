4b264d65c8c8982468f9935a07ac5c8e299b547d
barryetter <barryetter@gmail.com>
2025-07-04 09:39:08 -0400

    Map on contact view

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="container mt-5">
     <h1>Contact Us</h1>
-    <p>e-mail: WilliamWorley75@icloud.com</p>
+    <p>
+      e-mail:
+      <a href="mailto:WilliamWorley75@icloud.com">WilliamWorley75@icloud.com</a>
+    </p>
     <p>Phone: (304) 922-6523</p>
     <p>260 Autumn Lane, Princeton, WV 24739</p>
   </div>
   <div class="container mt-5">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d71803.07707822407!2d-81.1866939883687!3d37.31224991075589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x884e6b25ac6316f9%3A0xf1d879652ce15b!2s260%20Autumn%20Ln%2C%20Princeton%2C%20WV%2024739!5e0!3m2!1sen!2sus!4v1751574379135!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d71803.07707822407!2d-81.1866939883687!3d37.31224991075589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x884e6b25ac6316f9%3A0xf1d879652ce15b!2s260%20Autumn%20Ln%2C%20Princeton%2C%20WV%2024739!5e0!3m2!1sen!2sus!4v1751574379135!5m2!1sen!2sus"
+      width="600"
+      height="450"
+      style="border: 0"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
   </div>
 </template>
 <script setup></script>
